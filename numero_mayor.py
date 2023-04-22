a07409fbabd5de56f15bca268f6b0cde1a75d06a
ElTinMartin95 <131550247+ElTinMartin95@users.noreply.github.com>
2023-04-22 13:32:34 -0500

    Create numero_mayor.py
    ejercicio practico

---
+++ numero_mayor.py
@@ -0,0 +1,3 @@
+# Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
+
+# Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
